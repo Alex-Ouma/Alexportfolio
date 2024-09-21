4cb331b078f19145858d2651027577f2ec56adb0
alex ouma <alex2019ouma@gmail.com>
2024-09-21 10:04:50 +0300

    first commit

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-gray-100">
-    <h1 class="pl-8 py-4 text-xl font-bold">Freemano</h1>
+    <h1 class="pl-8 py-4 text-xl font-bold">ALEX BARASA</h1>
   </div>
 </template>
 <script setup lang="ts">
